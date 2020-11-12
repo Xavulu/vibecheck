10f3f6863517ed5ec82204e8d1c1d4cbe852dc43
Xavier Fernandez <lain@MacBook-Pro-3.home>
2020-11-12 08:32:07 -0500

    app is mostly done just need to figure out how to use tests on projects considered modules first

--- README.md
+++ README.md
@@ -1,6 +1,37 @@
 ![vibecheck logo](vibecheck.gif)
 
-### vibecheck (wip)
+## vibecheck 
 > a url verification microservice that may or may not be useful to anyone. 
-> use it for your url shortners or any other apis that allow user submitted urls.   
+> use it for your url shortners or any other aplications that allow user submitted urls. 
+
+### installation 
+
+First you should make a google cloud api account and get an authentication key for their [Safe Browsing Lookup API (v4)](https://developers.google.com/safe-browsing/v4/lookup-api), hold on to this and be smart about how you decide to store it (.env + .gitignore?). You also need to make sure you have redis on your pc (abd running) whether its the installed version or through a docker container.
+
+After doing this you can clone the repository and run yarn install to gather all the dependencies (its an insane amount cause of the javascript ecosystem lol). After this make a .env file at the root of the project with the following content: 
+``` 
+API_KEY={your api key here, omit the brackets and no quotes!!} 
+SERVER_PORT={port number of your choice}
+``` 
+After this you can start the application for testing by running yarn start from the root directory. The application effectively has one route because its only meant verify url schemes/test them against google's database of malicious urls. You can make requests against this route with curl like this: 
+``` 
+curl -d '{"url" : "http://testsafebrowsing.appspot.com/apiv4/ANY_PLATFORM/MALWARE/URL/"}' -H "Content-Type: application/json" -X POST http://localhost:3000/vibecheck 
+
+curl -d '{"url" : "https://www.google.com"}' -H "Content-Type: application/json" -X POST http://localhost:3000/vibecheck
+
+``` 
+The first request uses googles sample malicious url which yields this response: 
+
+``` 
+{"http://testsafebrowsing.appspot.com/apiv4/ANY_PLATFORM/MALWARE/URL/":true}
+``` 
+If you want to test other malicious urls against it you can use one from [openphish](https://openphish.com/) though there is no guarantee these are all indexed by google yet (Dont visit any of these urls by the way). A succesful response looks like this: 
+
+``` 
+{"https://www.google.com":false}
+```
+In both cases the boolean value denotes whether the url was malicious ; false meaning it was true meaning it is. 
+
+I also uncluded a vibecheck.json config file for running this application in a three node cluster in the background with [pm2](https://github.com/Unitech/pm2). Granted a process manager/load balancer written in javascript is probably not the best idea but its here if you want it. The app is also setup to scan for herokus redis environmental variable if you choose to host it that way and have rdis provisioned (I havent tested this out yet.....)
+
 
--- vibecheck.js
+++ vibecheck.js
@@ -1,8 +1,14 @@
-'use strict';
+'use strict'
+import { rejects } from 'assert';
+import { url } from 'inspector';
 import { createRequire } from 'module';
+import { resolve } from 'path';
 const require = createRequire(import.meta.url);
-
+const redis = require('redis');
+const axios = require('axios');
 require('dotenv').config();
+
+
 const lookup = require('safe-browse-url-lookup')({ apiKey: process.env.API_KEY});
 //make sure to have a .env file with a line like 
 //API_KEY={YOUR GOOGLE CLOUD API KEY FOR SAFE BROWSING URL}
@@ -11,25 +17,116 @@ const lookup = require('safe-browse-url-lookup')({ apiKey: process.env.API_KEY})
 //in case you don't refer to this and not me: https://developers.google.com/safe-browsing/v4#lookup-api-v4
 //so if you use this and get burned sorry ¯\_(ツ)_/¯
 
-const Keyv = require('keyv'); 
-const keyv = new Keyv();
 
+
+
+
+
+const {URL, parse} = require('url'); 
+
+const stringIsAValidUrl = (s, protocols) => {
+    try {
+        new URL(s);
+        const parsed = parse(s);
+        return protocols
+            ? parsed.protocol
+                ? protocols.map(x => `${x.toLowerCase()}:`).includes(parsed.protocol)
+                : false
+            : true;
+    } catch (err) {
+        return false;
+    }
+};
+
+
+
+////redis stuff
+const cache = redis.createClient(process.env.REDIS_URL || 6379);
+cache.on('error', function(err) {
+    console.error('error event - ' + client.host + ':' + client.port + ' - ' + err);
+  });
+
+
+//server stuff
 const Hapi = require('@hapi/hapi'); 
+
+
+///////////////////////////////////////
 
 const vibecheck = async() => {
 
     const server = Hapi.server({ 
-        port: 3000, 
-        host: 'localhost'
+        port: process.env.port || process.env.SERVER_PORT 
+        //host: 'localhost'
     }); 
+
+    await server.register({
+        plugin: require('hapi-pulse'),
+        options: {
+          timeout: 15000,
+          logger: console,
+          signals: ['SIGINT'],
+          postServerStop: async function () {
+            console.log('server stopped')
+            process.exitCode = 1;
+          }
+        }
+      })
+
+      server.route({
+        method: 'get',
+        path: '/',
+        handler: (request, h) => {
+            return {"yo" : "hello"}
+        }
+    });
+
+    server.route({
+        method: 'POST',
+        path: '/vibecheck',
+        handler: (request, h) => {
+            const urlpayload = request.payload;
+            const validate = stringIsAValidUrl(urlpayload["url"], ['http', 'https']);
+            if (validate === false){
+                let urlstr = String(urlpayload["url"]);
+                const noturl = { noturl : true};
+                //const nstr = JSON.stringify(noturl); //nstr means not-url string in case the abbreviation is confusing
+                return noturl;
+            };
+
+            cache.exists(urlpayload["url"], function(err, reply) {
+                if (reply === 1) {
+                    var cacheres //caheres = cache result
+                    cache.get(urlpayload["url"], function(err, reply){ 
+
+                        cacheres = JSON.parse(reply)});
+                    return cacheres; 
+                } 
+            });
+
+            let response = lookup.checkMulti([urlpayload["url"]])
+                .then(urlMap => {
+                    cache.setex(urlpayload["url"], 600, JSON.stringify(urlMap)); 
+                    return urlMap;
+                })
+                .catch(err => {
+                    console.log('Something went wrong.');
+                    console.log(err);
+                    let failed = urlpayload["url"];
+                    return {failed : "failed"}
+                });
+            let rstr = JSON.stringify(response);
+            cache.setex(urlpayload["url"], 600, rstr);
+            return response;
+        }
+        
+    });
 
     await server.start(); 
     console.log('Server running on %s', server.info.uri);
-    
 }; 
 
 process.on('unhandledRejection', (err) => {
-
     console.log(err); 
     process.exit(1);
 }); 
